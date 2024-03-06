99f171801ac47c07d3fbda021f2ba22914e6cebc
user.walid1921 <user.kouiderayadwalid@gmail.com>
2024-03-06 23:28:13 +0100

    changed the content

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -27,21 +27,20 @@ const ImageSlider = ({ projects, openProject }) => {
       <div className="embla__viewport " ref={emblaRef}>
         <div className="embla__container xl:gap-[160px] gap-8">
           {projects.map((project) => (
-            <Link
+            <div
               className=" embla__slide relative xl:h-[520px] xl:w-[850px] h-[250px] imageFade hover:cursor-grab transition-all ease-in-out duration-300"
               key={project.id}
-              to="/gallery"
             >
               <img
                 className="embla__slide__img"
                 src={project.img}
-                alt="Your alt text"
-                onClick={() => openProject(project.id)}
+                alt={project.title}
               />
-              <button className=" w-full h-full text-[#bbb] bg-black opacity-0 absolute bottom-0 right-0 z-1 hover:bg-black/60 hover:opacity-80 cursor-pointer transition-all ease-in-out duration-500 text-xl hover:text-2xl font-semibold ">
+
+              <span className="  text-[#bbb] absolute md:bottom-5 bottom-2 md:right-10 right-3 z-1 md:text-xl font-semibold ">
                 {project.title}
-              </button>
-            </Link>
+              </span>
+            </div>
           ))}
         </div>
       </div>
